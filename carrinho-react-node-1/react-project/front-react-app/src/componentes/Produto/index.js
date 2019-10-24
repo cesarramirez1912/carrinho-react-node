aa3cc82c810f94bc659bdb7fd4761df4c9091e67
Cesar Ruben Gauto Ramirez <Ramirez-1912@hotmail.com>
2019-10-24 12:36:03 -0300

    commit modificacao MostrarProdutos

--- carrinho-react-node-1/react-project/front-react-app/src/componentes/Produto/index.js
+++ carrinho-react-node-1/react-project/front-react-app/src/componentes/Produto/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Button,Table,InputGroup,Input,Col,Row} from 'reactstrap';
 import './style.css';
-
+import { FaCartPlus,FaEdit,FaTrashAlt } from "react-icons/fa";
 
 export class MostrarProdutos extends Component {
 
@@ -17,9 +17,7 @@ export class MostrarProdutos extends Component {
     }
 
     render(){
-
         var items = this.props.controle.produtos;
-   
         return (
             <Table striped className="estilo-tabela">
               <thead className="bg-dark text-white">
@@ -35,20 +33,19 @@ export class MostrarProdutos extends Component {
                      items.map((item,index)=> (
                         <tr key={item.id_produto}>
                               <td>
-                                 {item.id_produto}
+                               <h6>{item.id_produto}</h6>
                                    </td>
                                    <td>
-                                       {item.descricao}
+                                       <h6>{item.descricao}</h6>
                                    </td>
                                    <td>
-                                       {item.preco}
+                                      <h6>{item.preco}</h6>
                                    </td>
                                    <td>
-                                      <Button color="info"  onClick={event => this.props.eventoEditarProduto({id_produto:item.id_produto,descricao:item.descricao,preco:item.preco,index})} size="sm">editar</Button>{' '}
-                                      <Button color="danger" onClick={event => this.props.eventoRemoveProduto(item.id_produto,index)} size="sm">deletar</Button>{' '}
-                                      <Button color="success" onClick={event => this.props.eventoAddCarrinho(item.id_produto)} size="sm">carrinho</Button>
+                                      <Button color="info"  onClick={event => this.props.eventoEditarProduto({id_produto:item.id_produto,descricao:item.descricao,preco:item.preco,index})} size="sm"><FaEdit/></Button>{' '}
+                                      <Button color="danger" onClick={event => this.props.eventoRemoveProduto(item.id_produto,index)} size="sm"><FaTrashAlt/></Button>{' '}
+                                      <Button color="success" onClick={event => this.props.eventoAddCarrinho(event,({descricao:item.descricao,preco:item.preco,quantidade:0,precoTotal:item.preco}))} size="sm"><FaCartPlus/></Button>
                                    </td>
-                                   
                                 </tr>
                            ))
              }
@@ -128,7 +125,7 @@ export class FormularioProduto extends Component{
                     <InputGroup className="mb-2">
                         <Input name="qntd" defaultValue={this.props.controle.inputDefaultId} onChange={(event)=> this.trocarQuantidade(event)} placeholder="1,2,3.." />
                     </InputGroup>
-                    {this.props.controle.botaoCadastrar ? <Button className="w-100 mt-3 bg-success" onClick={(event)=> this.props.eventoAddProduto(event)}>Cadastrar</Button> : <Button className="w-100 mt-3 bg-info" onClick={this.clickEdit}>Registrar</Button>}
+                    {this.props.controle.botaoCadastrar ? <Button className="w-100 mt-3 bg-success" onClick={(event)=> this.props.eventoAddProduto(event)}>Cadastrar</Button> : <Button className="w-100 mt-3 bg-info" onClick={this.clickEdit}>Atualizar</Button>}
                 </form>
                </Col>
               </Row>
